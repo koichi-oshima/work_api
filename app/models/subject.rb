85d61be021a50e79a3099962928ae8b99b2771fd
koichi-oshima <fairemygeneration@yahoo.co.jp>
2020-05-05 18:50:37 +0900

    add json format

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,21 +4,39 @@ class Subject < ApplicationRecord
 
   def self.search(keyword, teacher_name)
 
-    Subject.joins(:teacher, :lectures)
-    .select("
-      subjects.id,
-      subjects.title,
-      subjects.weekday,
-      subjects.period,
-      teachers.id as teacher_id,
-      teachers.name as teacher_name,
-      lectures.id as lecture_id,
-      lectures.title as lecture_title,
-      lectures.date as lecture_date
-    ")
-    .where("subjects.title LIKE ? AND teachers.name LIKE ? ", "%#{keyword}%", "%#{teacher_name}%")
-    .order("subjects.id")
+    #@temp = Subject.joins(:teacher, :lectures)
+    #.select("
+    #  subjects.id,
+    #  subjects.title,
+    #  subjects.weekday,
+    #  subjects.period,
+    #  teachers.id as teacher_id,
+    #  teachers.name as teacher_name,
+    #  lectures.id as lecture_id,
+    #  lectures.title as lecture_title,
+    #  lectures.date as lecture_date
+    #")
+    #.where("subjects.title LIKE ? AND teachers.name LIKE ? ", "%#{keyword}%", "%#{teacher_name}%")
+    #.order("subjects.id")
+
+
+    Subject.preload(:teacher, :lectures)
+      .as_json(
+        :only => [:id, :title, :weekday, :period],
+          include:
+          [
+            {
+              teacher: {
+                only: [:id, :name]
+              }
+            },
+            {
+              lectures: {
+                only: [:id, :title, :date]
+              }
+            }
+          ]
+      )
 
   end
-
 end
